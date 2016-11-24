dac38ce90b5b772e1fb5b18ac7fc95c7e36d10de
ronald <ronald@test.nl>
2016-11-24 14:47:06 +0100

    Begin aanaken Question class

--- include/classes/cls.Question.php
+++ include/classes/cls.Question.php
@@ -1,7 +1,36 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Name: Ronald van der Weide
+ * Date: 24/11/2016
  */
+
+Class Question extends DAL {
+
+    private $questionID;
+
+    public function __construct($questionID) {
+        parent::__construct();
+
+        $this->QuestionID = $questionID;
+    }
+    /**
+     * Method for retrieving question data
+     * 
+     * @param int $questionID
+     * @return object
+     */
+    public function getQuestion($questionID)
+    {
+        $sql = "SELECT *
+                FROM QUESTION
+                WHERE QuestionID = :questionid";
+        $result = $this->query($sql, array(
+            ":questionid" => array($questionID, PDO::PARAM_INT)
+        ), "one");
+
+        return $result;
+    }
+}
+
 ?>
