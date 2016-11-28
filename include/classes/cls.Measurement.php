381f18a1032249b7298b5d61b9b50a35cf4ab6e6
Patrick Pieper <ppieper96@gmail.com>
2016-11-28 12:50:09 +0100

    Created functions getMeasurement and createMeasurement.

--- include/classes/cls.Measurement.php
+++ include/classes/cls.Measurement.php
@@ -15,12 +15,42 @@ class Measurement extends DAL
 		parent::__construct();
 	}
 
-	public function getMeasurement(measurementID) 
+	/**
+	 * Gets a measurent by its ID.
+	 * @param  int $measurementID [description]
+	 * @return [type]                [description]
+	 */
+	public function getMeasurement($measurementID) 
 	{
 		$sql = "SELECT *
 				FROM MEASUREMENT
-				WHERE MeasurementID = measurementID";
+				WHERE MeasurementID =".":measurementID";
 
+		$result = $this->query($sql, array(
+			":measurementID" => array($measurementID, PDO::PARAM_INT)), "one");
+
+		return $result;
 	}
+	
+	/**
+	 * Create new measurement and add it to database.
+	 * @param  string $name        name of the measurement.
+	 * @param  int $treatmentID    id of the treatment that is linked to measurement.
+	 * @param  int $questionlistID id of the questionlist that needs to be filled in within this measurement.
+	 * @return int insertedID	   Returns the id of the inserted row. 				                   
+	 */
+	public function create_Measurement($name, $treatmentID, $questionlistID)
+	{
+		$sql = "INSERT INTO MEASUREMENT (Number, Name, TreatmentID, QuestionlistID) 
+				VALUES (:number, :name, :treatmentID, :questionlistID)";
+
+		$result = $this->query($sql, array(
+			":name" => array($name, PDO::PARAM_STRING),
+			":treatmentID" => array($treatmentID, PDO::PARAM_INT),
+			"questionlistID" => array($questionlistID, PDO::PARAM_INT)));
+
+		return $result;
+	}
+
 }
 
